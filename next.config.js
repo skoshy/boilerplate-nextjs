a4665994fceae58decbd433ee96c21b1dc48dcc6
Stefan Koshy <noreply@skoshy.com>
2019-12-19 10:58:16 -0500

    Fix CSS parsing issue

--- next.config.js
+++ next.config.js
@@ -1,51 +1,54 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
+const withCss = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 
 const isDev = process.env.NODE_ENV === "development";
 
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 2,
-    localIdentName: "[local]___[hash:base64:5]"
-  },
+module.exports = withCss(
+  withSass({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 2,
+      localIdentName: "[local]___[hash:base64:5]"
+    },
 
-  exportTrailingSlash: !isDev,
+    exportTrailingSlash: !isDev,
 
-  webpack(config) {
-    // needed to allow for root imports
-    config.resolve.modules.push(path.resolve("./"));
+    webpack(config) {
+      // needed to allow for root imports
+      config.resolve.modules.push(path.resolve("./"));
 
-    // add SASS global + module support - https://github.com/zeit/next-plugins/issues/149#issuecomment-526881653
-    config.module.rules.forEach(rule => {
-      if (rule.test.test(".sass") || rule.test.test(".scss")) {
-        rule.rules = rule.use.map(useRule => {
-          if (typeof useRule === "string") {
-            return { loader: useRule };
-          }
+      // add SASS global + module support - https://github.com/zeit/next-plugins/issues/149#issuecomment-526881653
+      config.module.rules.forEach(rule => {
+        if (rule.test.test(".sass") || rule.test.test(".scss")) {
+          rule.rules = rule.use.map(useRule => {
+            if (typeof useRule === "string") {
+              return { loader: useRule };
+            }
 
-          if (useRule.loader.startsWith("css-loader")) {
-            return {
-              oneOf: [
-                {
-                  test: /\.module\.s[ac]ss$/,
-                  loader: useRule.loader,
-                  options: useRule.options
-                },
-                {
-                  loader: useRule.loader,
-                  options: {}
-                }
-              ]
-            };
-          }
-          return useRule;
-        });
-        delete rule.use;
-      }
-    });
+            if (useRule.loader.startsWith("css-loader")) {
+              return {
+                oneOf: [
+                  {
+                    test: /\.module\.s[ac]ss$/,
+                    loader: useRule.loader,
+                    options: useRule.options
+                  },
+                  {
+                    loader: useRule.loader,
+                    options: {}
+                  }
+                ]
+              };
+            }
+            return useRule;
+          });
+          delete rule.use;
+        }
+      });
 
-    return config;
-  }
-});
+      return config;
+    }
+  })
+);
