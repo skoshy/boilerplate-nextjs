abc9d5897061d99c91575f8902fb1f5fafe5255c
Stefan Koshy <noreply@skoshy.com>
2019-12-25 16:09:32 -0500

    Add some basic styling for pages

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,4 +5,17 @@ const MainLayout = ({ children, className }) => (
   <div className={`layout main ${className}`}>{children}</div>
 );
 
-export default withCss(MainLayout, css => css``);
+export default withCss(
+  MainLayout,
+  css => css`
+    & {
+      border: 1px solid #eee;
+      box-shadow: 0px 0px 10px #ddd;
+      padding: 2rem;
+      margin-top: 2rem;
+      margin-left: 2rem;
+      margin-right: 2rem;
+      height: 100%;
+    }
+  `
+);
