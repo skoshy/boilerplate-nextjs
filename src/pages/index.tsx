b29f1d3cfbff3796a6a05973ca90545c832db871
Stefan Koshy <noreply@skoshy.com>
2020-01-09 03:18:22 -0500

    Test with using router to get query params

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useRouter } from "next/router";
 import MainLayout from "src/layouts/MainLayout";
 import { withCss } from "src/lib/withCss";
 import "src/styles/global.scss";
 import css from "./index.module.scss";
 
-const Page = () => (
-  <MainLayout>
-    <div className={css.header}>Howdy</div>
-  </MainLayout>
-);
+const Page = () => {
+  const router = useRouter();
+  const query = router?.query ?? {};
+
+  return (
+    <MainLayout>
+      <div className={css.header}>Howdy</div>
+      {Object.keys(query).length ? JSON.stringify(query) : null}
+    </MainLayout>
+  );
+};
 
 export default withCss(Page, css => css``);
