53aa74ced47216e5aae3b28c3da3de1210dadeef
Stefan Koshy <noreply@skoshy.com>
2020-01-10 01:11:57 -0500

    Add favicon

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -26,6 +26,7 @@ const UnstyledMainLayout = ({
         href="https://fonts.googleapis.com/css?family=Muli:400,700&display=swap"
         rel="stylesheet"
       />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
     <Box
       border
