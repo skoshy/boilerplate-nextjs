4b209be2f1f20ca77296149d6fc097cd6c1ff9a3
Stefan Koshy <noreply@skoshy.com>
2020-01-23 17:54:58 -0500

    Add justify-items utilities to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,41 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addUtilities, variants }) {
+      const prefix = '.justify-items';
+      const key = 'justifyItems';
+
+      const arr = [
+        'baseline',
+        'center',
+        'end',
+        'flex-end',
+        'flex-start',
+        'inherit',
+        'initial',
+        'left',
+        'legacy',
+        'normal',
+        'right',
+        'safe',
+        'self-end',
+        'self-start',
+        'start',
+        'stretch',
+        'unsafe',
+        'unset',
+      ];
+
+      const newUtilities = arr.reduce((acc, val) => {
+        acc[`${prefix}-${val}`] = {
+          [key]: val,
+        };
+
+        return acc;
+      }, {});
+
+      addUtilities(newUtilities, variants('customPlugin'));
+    },
+  ],
 };
