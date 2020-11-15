88b5e483712c4ac1c4e3940eaefe32fe317149db
Stefan Koshy <noreply@skoshy.com>
2020-11-15 15:31:36 -0500

    Modify date endpoint

--- src/pages/api/example/date.tsx
+++ src/pages/api/example/date.tsx
@@ -1,5 +1,5 @@
 export default function handler(req, res): void {
-  res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
   res.end(`${Date.now()}`);
 }
