cb1481b43c98fa54fe2ed6855e389f88d9d2079c
Stefan Koshy <noreply@skoshy.com>
2020-11-15 15:46:29 -0500

    Use brand colors for select

--- src/components/_lib_/Select.tsx
+++ src/components/_lib_/Select.tsx
@@ -2,7 +2,7 @@ import 'twin.macro';
 
 export const Select = () => {
   return (
-    <select tw="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-indigo-500 pl-3 pr-10">
+    <select tw="rounded border appearance-none border-brand-300 py-2 focus:outline-none focus:border-brand-500 pl-3 pr-10">
       <option>SM</option>
       <option>M</option>
       <option>L</option>
