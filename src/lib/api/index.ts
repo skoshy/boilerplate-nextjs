912443d1fedf7e8874a58bf214dc391909fde108
Stefan Koshy <noreply@skoshy.com>
2020-11-25 08:53:12 -0500

    Remove explicit insert users call

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -9,12 +9,6 @@ export const signUp = async (email, password) => {
 
   const id = signUpResponse.user.id;
 
-  const insertResponse = await supabase.public
-    .from('users')
-    .insert([
-      { id, email },
-    ]);
-
   return {
     id,
     email,
